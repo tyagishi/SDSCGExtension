2a504ce0b80752ec819e10b61475a7d92fa016bd
Tomoaki Yagishita <tyagishi@gmail.com>
2021-03-10 19:36:40 +0900

    rename add moveBottomLeftCornerToNewWidthKeepingSizeRatio and moveBottomLeftCornerToNewHeightKeepingSizeRatio add tests

--- Sources/SDSCGExtension/CGRect-ExtensionCornerAdjustment.swift
+++ Sources/SDSCGExtension/CGRect-ExtensionCornerAdjustment.swift
@@ -8,13 +8,25 @@
 import Foundation
 
 extension CGRect {
-    public func adjustWidthKeepingSizeRatio(_ newWidth: CGFloat) -> CGRect {
+    public func moveBottomRightCornerToNewWidthKeepingSizeRatio(_ newWidth: CGFloat) -> CGRect {
         let newHeight = self.height * newWidth / self.width
         return CGRect(origin: self.origin, size: CGSize(width: newWidth, height: newHeight))
     }
     
-    public func adjustHeightKeepingSizeRatio(_ newHeight: CGFloat) -> CGRect {
+    public func moveBottomRightCornerToNewHeightKeepingSizeRatio(_ newHeight: CGFloat) -> CGRect {
         let newWidth = self.width * newHeight / self.height
         return CGRect(origin: self.origin, size: CGSize(width: newWidth, height: newHeight))
+    }
+    
+    public func moveBottomLeftCornerToNewWidthKeepingSizeRatio(_ newWidth: CGFloat ) -> CGRect {
+        let newHeight = self.height * newWidth / self.width
+        let newOrigin = CGPoint(x: self.origin.x + (self.width - newWidth), y: self.origin.y)
+        return CGRect(origin: newOrigin, size: CGSize(width: newWidth, height: newHeight))
+    }
+    
+    public func moveBottomLeftCornerToNewHeightKeepingSizeRatio(_ newHeight: CGFloat ) -> CGRect {
+        let newWidth = self.width * newHeight / self.height
+        let newOrigin = CGPoint(x: self.origin.x + (self.width - newWidth), y: self.origin.y)
+        return CGRect(origin: newOrigin, size: CGSize(width: newWidth, height: newHeight))
     }
 }
--- Tests/SDSCGExtensionTests/SDSCGExtensionTests.swift
+++ Tests/SDSCGExtensionTests/SDSCGExtensionTests.swift
@@ -4,21 +4,39 @@ import XCTest
 final class SDSCGExtensionTests: XCTestCase {
     
     
-    func test_adjustWidthKeepingSizeRatio_withoutMovingOrigin_onlySizeShouldBeAdjusted() throws {
+    func test_moveBottomRightCornerToNewWidthKeepingSizeRatio_NA_onlySizeShouldBeAdjusted() throws {
         // (10,20) - (40,80)
         let rect = CGRect(x: 10, y: 20, width: 30, height: 60)
-        let newRect = rect.adjustWidthKeepingSizeRatio(45 as CGFloat)
-        XCTAssertEqual(rect.origin, newRect.origin)
+        let newRect = rect.moveBottomRightCornerToNewWidthKeepingSizeRatio(45 as CGFloat)
+        XCTAssertEqual(newRect.origin, rect.origin)
         XCTAssertEqual(newRect.size.width, 45 as CGFloat)
         XCTAssertEqual(newRect.size.height, 90 as CGFloat)
     }
     
-    func test_adjustHeightKeepingSizeRatio_withoutMovingOrigin_onlySizeShouldBeAdjusted() throws {
+    func test_moveBottomRightCornerToNewHeightKeepingSizeRatio_NA_onlySizeShouldBeAdjusted() throws {
         let rect = CGRect(x: 10, y: 20, width: 30, height: 60)
-        let newRect = rect.adjustHeightKeepingSizeRatio(40 as CGFloat)
-        XCTAssertEqual(rect.origin, newRect.origin)
+        let newRect = rect.moveBottomRightCornerToNewHeightKeepingSizeRatio(40 as CGFloat)
+        XCTAssertEqual(newRect.origin, rect.origin)
         XCTAssertEqual(newRect.size.width, 20 as CGFloat)
         XCTAssertEqual(newRect.size.height, 40 as CGFloat)
+    }
+
+    func test_moveBottomLeftCornerToNewWidthKeepingSizeRatio_NA_SizeAndOriginXShouldBeAdjusted() throws {
+        let rect = CGRect(x: 10, y: 20, width: 30, height: 60)
+        let newRect = rect.moveBottomLeftCornerToNewWidthKeepingSizeRatio(45 as CGFloat)
+        XCTAssertEqual(newRect.origin.x, -5)
+        XCTAssertEqual(newRect.origin.y, 20)
+        XCTAssertEqual(newRect.size.width, 45 as CGFloat)
+        XCTAssertEqual(newRect.size.height, 90 as CGFloat)
+    }
+
+    func test_moveBottomLeftCornerToNewHeightKeepingSizeRatio_NA_SizeAndOriginXShouldBeAdjusted() throws {
+        let rect = CGRect(x: 10, y: 20, width: 30, height: 60)
+        let newRect = rect.moveBottomLeftCornerToNewHeightKeepingSizeRatio(90 as CGFloat)
+        XCTAssertEqual(newRect.origin.x, -5)
+        XCTAssertEqual(newRect.origin.y, 20)
+        XCTAssertEqual(newRect.size.width, 45 as CGFloat)
+        XCTAssertEqual(newRect.size.height, 90 as CGFloat)
     }
 
 }
