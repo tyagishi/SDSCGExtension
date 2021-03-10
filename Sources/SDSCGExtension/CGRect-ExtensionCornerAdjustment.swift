af53aab8c4afafd5a31027ca1e04db2fe7fe877c
Tomoaki Yagishita <tyagishi@gmail.com>
2021-03-10 19:20:25 +0900

    make public

--- Sources/SDSCGExtension/CGRect-ExtensionCornerAdjustment.swift
+++ Sources/SDSCGExtension/CGRect-ExtensionCornerAdjustment.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 extension CGRect {
-    func adjustWidthKeepingSizeRatio(_ newWidth: CGFloat) -> CGRect {
+    public func adjustWidthKeepingSizeRatio(_ newWidth: CGFloat) -> CGRect {
         let newHeight = self.height * newWidth / self.width
         return CGRect(origin: self.origin, size: CGSize(width: newWidth, height: newHeight))
     }
     
-    func adjustHeightKeepingSizeRatio(_ newHeight: CGFloat) -> CGRect {
+    public func adjustHeightKeepingSizeRatio(_ newHeight: CGFloat) -> CGRect {
         let newWidth = self.width * newHeight / self.height
         return CGRect(origin: self.origin, size: CGSize(width: newWidth, height: newHeight))
     }
